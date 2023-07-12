5c095eee504f8ba341c2afd5e51f2c8799e8f738
faustojc <faustojcboko@gmail.com>
2023-07-12 18:02:31 +0800

    Rename the class

--- application/modules/register/models/service/Register_services_model.php
+++ application/modules/register/models/service/Register_services_model.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-class Add_personnel_services_model extends CI_Model
+
+class Register_services_model extends CI_Model
 {
     public $ID;
     public $Table;
@@ -19,22 +20,23 @@ class Add_personnel_services_model extends CI_Model
         $this->Table = json_decode(TABLE);
     }
 
-    public function save(){
-        try{
-            if(empty($this->LName)|| empty($this->MName) ||
-            empty($this->FName)){
+    public function save()
+    {
+        try {
+            if (empty($this->LName) || empty($this->MName) ||
+                empty($this->FName)) {
                 throw new Exception(MISSING_NAME_DETAILS, true);
             }
-            if(empty($this->Username)){
+            if (empty($this->Username)) {
                 throw new Exception(MISSING_USERNAME, true);
             }
-            if(empty($this->Email) || empty($this->Department) || empty($this->Position)){
+            if (empty($this->Email) || empty($this->Department) || empty($this->Position)) {
                 throw new Exception(MISSING_DETAILS, true);
             }
             $pass = "123456";
             $Locker = locker();
             $Password = sha1(password_generator($pass, $Locker));
-            
+
             $data = array(
                 'FName' => $this->FName,
                 'MName' => $this->MName,
@@ -48,21 +50,20 @@ class Add_personnel_services_model extends CI_Model
             );
 
             $this->db->trans_start();
-            $this->db->insert($this->Table->personnel,$data);
-            $insert_id = $this->db->insert_id();          
+            $this->db->insert($this->Table->personnel, $data);
+            $insert_id = $this->db->insert_id();
 
             $this->db->trans_complete();
-            if ($this->db->trans_status() === FALSE)
-            {                
+            if ($this->db->trans_status() === FALSE) {
                 $this->db->trans_rollback();
-                throw new Exception(ERROR_PROCESSING, true);	
-            }else{
-                if($this->Department=="OBO" && $this->Position==3){
+                throw new Exception(ERROR_PROCESSING, true);
+            } else {
+                if ($this->Department == "OBO" && $this->Position == 3) {
 
                     // $insp = explode(',',$this->Insp_Type);
                     $count = sizeof($this->Insp_Type);
                     $x = 0;
-    
+
                     // while($x <= $count){
                     //     if(!empty($lab[$x])){
                     //     // $final_result = array(
@@ -71,10 +72,10 @@ class Add_personnel_services_model extends CI_Model
                     //     //     'Request_id'    => $last_id,
                     //     //     'DateTaken'   => date('Y-m-d H:i:s')
                     //     // );
-    
+
                     //     // $this->db->insert($this->Table->request_item,$final_result);
                     //     // }
-            
+
                     //     // $update_cost = array(
                     //     //     'Totalbill' => $sum
                     //     // );
@@ -89,26 +90,24 @@ class Add_personnel_services_model extends CI_Model
                     //     }
                     // }
                     // }
-                    if(empty($Insp_Type) && $this->Position == "3"){
+                    if (empty($Insp_Type) && $this->Position == "3") {
                         throw new Exception(MISSING_INSP, true);
-                    }
-                    elseif (!empty($Insp_Type)) {
+                    } elseif (!empty($Insp_Type)) {
                         foreach ($this->Insp_Type as $x => $Insp_Type) {
-                        
+
                             $insp_data = array(
                                 'InspectionID' => $Insp_Type,
                                 'PersonnelID' => $insert_id
                             );
                             $this->db->insert($this->Table->obo_personnel, $insp_data);
                         }
-                    }                   
-                $this->db->trans_commit();
-                return array('message'=>SAVED_SUCCESSFUL, 'has_error'=>false);
+                    }
+                    $this->db->trans_commit();
+                    return array('message' => SAVED_SUCCESSFUL, 'has_error' => false);
+                }
             }
-            }
-        }
-        catch(Exception$msg){
-            return (array('message'=>$msg->getMessage(), 'has_error'=>true));
+        } catch (Exception$msg) {
+            return (array('message' => $msg->getMessage(), 'has_error' => true));
         }
     }
 }

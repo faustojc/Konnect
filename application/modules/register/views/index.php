98945163f6374661faec70c7e135dea6a44b14a7
faustojc <faustojcboko@gmail.com>
2023-07-12 17:59:54 +0800

    Fixed the layout and changed the image

--- application/modules/register/views/index.php
+++ application/modules/register/views/index.php
@@ -3,13 +3,10 @@
 <body class="fade-in d-flex align-items-center h-100">
     <div class="container">
         <div class="row py-5 mt-4 align-items-center">
-            <!-- For Demo Purpose -->
             <div class="col-md-5 pr-lg-5 mb-4 mb-md-0">
-                <img src="https://bootstrapious.com/i/snippets/sn-registeration/illustration.svg" alt="" class="img-fluid mb-3 d-none d-md-block">
+                <img src="<?= base_url() ?>assets/images/register/5423351_register.svg" alt="Create an account" class="img-fluid mb-3 d-none d-md-block">
                 <h1>Create an Account</h1>
-                <p class="font-italic text-muted mb-0">Create a minimal registration page using Bootstrap 4 HTML form elements.</p>
-                
-                </p>
+                <p class="font-italic text-muted mb-0">Join to Konnect community and connect to other jobseekers and employers</p>
             </div>
 
             <!-- Registration Form -->
@@ -26,8 +23,8 @@
                                             <span class="input-group-text bg-white px-4 border-md border-right-0">
                                                 <i class="fa fa-user text-muted"></i>
                                             </span>
+                                            <input id="email" type="email" name="email" placeholder="Enter email" class="form-control bg-white border-left-0 border-md" required>
                                         </div>
-                                        <input id="email" type="email" name="email" placeholder="Enter email" class="form-control bg-white border-left-0 border-md">
                                     </div>
 
                                     <!-- Password -->
@@ -36,26 +33,24 @@
                                             <span class="input-group-text bg-white px-4 border-md border-right-0">
                                                 <i class="fa fa-lock text-muted"></i>
                                             </span>
+                                            <input id="password" type="password" name="password" placeholder="Enter password" class="form-control bg-white border-left-0 border-md" required>
                                         </div>
-                                        <input id="password" type="password" name="password" placeholder="Enter password" class="form-control bg-white border-left-0 border-md">
                                     </div>
 
                                     <!-- User Type Dropdown -->
                                     <div class="input-group mb-4 py-2 px-2">
-                                        <select id="user_type" name="user_type" class="form-control custom-select bg-white border-left-0 border-md">
+                                        <select id="user_type" name="user_type" class="form-control custom-select bg-white border-md">
                                             <option selected disabled>-- Choose user type --</option>
                                             <option value="EMPLOYER">EMPLOYER</option>
                                             <option value="EMPLOYEE">EMPLOYEE</option>
                                         </select>
                                     </div>
 
-                                    <!-- Next Button -->
-                                    
                                 </div>
-                                <div class="text-right mt-3">
-                                        <a href="#" class="btn btn-info">
-                                            Next <i class="fas fa-chevron-right"></i>
-                                        </a>
+                                <div class="mt-3">
+                                    <a href="#" class="btn btn-info d-block">
+                                        Next <i class="fas fa-chevron-right"></i>
+                                    </a>
                                 </div>
                             </div>
                         </div>

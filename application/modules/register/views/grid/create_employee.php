3f03537797d7d126bf65dba6fd6437fd9da83c79
faustojc <faustojcboko@gmail.com>
2023-07-13 01:44:08 +0800

    Added name attribute to inputs

--- application/modules/register/views/grid/create_employee.php
+++ application/modules/register/views/grid/create_employee.php
@@ -1,30 +1,30 @@
-<form>
+<form id="needs-validation">
     <div class="row pb-3">
         <div class="col-md-4">
             <label>First Name</label>
-            <input type="text" class="form-control" id="Fname" style=" font-size:14px">
+            <input type="text" class="form-control" name="Fname" id="Fname" style=" font-size:14px">
         </div>
 
         <div class="col-md-4">
             <label>Middle Name</label>
-            <input type="text" class="form-control" id="Mname" style=" font-size:14px">
+            <input type="text" class="form-control" name="Mname" id="Mname" style=" font-size:14px">
         </div>
 
         <div class="col-md-4">
             <label>Last Name</label>
-            <input type="text" class="form-control" id="Lname" style=" font-size:14px">
+            <input type="text" class="form-control" name="Lname" id="Lname" style=" font-size:14px">
         </div>
     </div>
 
     <div class="row pb-3">
         <div class="col-md-6">
             <label>Contact Number</label>
-            <input type="number" class="form-control" id="Cnum">
+            <input type="number" class="form-control" name="Cnum" id="Cnum">
         </div>
 
         <div class="col-md-6">
             <label>Birth Date</label>
-            <input type="date" class="form-control" id="Bday" name="bday">
+            <input type="date" class="form-control" name="Bday" id="Bday">
         </div>
     </div>
 
@@ -32,31 +32,31 @@
     <div class="row pb-3">
         <div class="col-md-6">
             <label>Address</label>
-            <input type="text" class="form-control" id="Address" style=" font-size:14px">
+            <input type="text" class="form-control" name="Address" id="Address" style=" font-size:14px">
         </div>
 
         <div class="col-md-6">
             <label>Barangay</label>
-            <input type="text" class="form-control" id="Barangay" style=" font-size:14px">
+            <input type="text" class="form-control" name="Barangay" id="Barangay" style=" font-size:14px">
         </div>
     </div>
 
     <div class="row pb-3">
         <div class="col-md-6">
             <label>City</label>
-            <input type="text" class="form-control" id="City" style=" font-size:14px">
+            <input type="text" class="form-control" name="City" id="City" style=" font-size:14px">
         </div>
 
         <div class="col-md-6">
             <label>Religion</label>
-            <input type="text" class="form-control" id="Religion" style=" font-size:14px">
+            <input type="text" class="form-control" name="Religion" id="Religion" style=" font-size:14px">
         </div>
     </div>
 
     <div class="row pb-3">
         <div class="col-md-6">
             <label>Gender</label>
-            <select name="gender" class="form-control" id="Gender">
+            <select class="form-control" name="Gender" id="Gender">
                 <option value="male">Male</option>
                 <option value="female">Female</option>
                 <option value="others">Others</option>
--- application/modules/register/views/grid/create_employer.php
+++ application/modules/register/views/grid/create_employer.php
@@ -1,43 +1,43 @@
-<form>
+<form id="needs-validation">
     <div class="row pb-3">
         <div class="col-md-6">
-            <label>Employer Name</label>
-            <input type="text" class="form-control" id="employer_name" style=" font-size:14px">
+            <label for="employer_name">Employer Name</label>
+            <input type="text" class="form-control" name="employer_name" id="employer_name" style=" font-size:14px">
         </div>
 
         <div class="col-md-6">
-            <label>Trade Name</label>
-            <input type="text" class="form-control" id="tradename" style=" font-size:14px">
+            <label for="tradename">Trade Name</label>
+            <input type="text" class="form-control" name="tradename" id="tradename" style=" font-size:14px">
         </div>
 
     </div>
 
     <div class="row pb-3">
         <div class="col-md-6">
-            <label>Address</label>
-            <input type="text" class="form-control" id="Address" style=" font-size:14px">
+            <label for="address">Address</label>
+            <input type="text" class="form-control" name="address" id="address" style=" font-size:14px">
         </div>
 
         <div class="col-md-6">
-            <label>Barangay</label>
-            <input type="text" class="form-control" id="Barangay" style=" font-size:14px">
+            <label for="barangay">Barangay</label>
+            <input type="text" class="form-control" name="barangay" id="barangay" style=" font-size:14px">
         </div>
     </div>
 
     <div class="row pb-3">
         <div class="col-md-6">
-            <label>Contact Number</label>
-            <input type="number" class="form-control" id="Cnum">
+            <label for="contact_number">Contact Number</label>
+            <input type="number" class="form-control" name="contact_number" id="contact_number">
         </div>
         <div class="col-md-6">
-            <label>City</label>
-            <input type="text" class="form-control" id="City" style=" font-size:14px">
+            <label for="city">City</label>
+            <input type="text" class="form-control" name="city" id="city" style=" font-size:14px">
         </div>
     </div>
 
     <div class="row pb-3">
         <div class="col-md-12 d-flex justify-content-center">
-            <a href="#" type="submit" class="btn btn-info btn-block" id="btn_register">Register</a>
+            <a href="#" type="submit" class="btn btn-info btn-block" id="register">Register</a>
         </div>
     </div>
 </form>

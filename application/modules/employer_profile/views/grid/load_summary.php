dd1f96b3ab549dbb1356c56c4f093839209bca53
polcuenca <115073490+polcuenca@users.noreply.github.com>
2023-07-15 21:40:31 +0800

    edited UI if employer summary is not available

--- application/modules/employer_profile/views/grid/load_summary.php
+++ application/modules/employer_profile/views/grid/load_summary.php
@@ -1,15 +1,15 @@
-<?php if (empty($current_employer->summary)) : ?>
+<?php if (empty($current_employer->summary)): ?>
     <div class="d-flex flex-column flex-grow-1 border rounded p-3">
         <div class="d-flex align-items-center mb-2">
-            <i class="fa fa-address-book fa-3x"></i>
+            <i class="fa fa-address-book fa-2x"></i>
             <h5 class=" ml-3">Write a summary to highlight your company</h5>
         </div>
         <div class="d-flex flex-column flex-grow-1">
             <p class="fs-14">A well-written summary can attract more qualified and interested applicants, as well as showcase your brand identity and reputation.</p>
-            <button type="button" class="btn btn-outline-secondary rounded-pill bg-gray-light edit-summary" style="border-width: 2px" data-toggle="modal" data-target="#summary-modal">Add a summary</button>
+            <button type="button" class="btn btn-light rounded-pill edit-summary" style="border-width: 2px" data-toggle="modal" data-target="#summary-modal">Add a summary</button>
         </div>
     </div>
-<?php else : ?>
+<?php else: ?>
     <div class="summary-content">
         <?= $current_employer->summary ?>
     </div>

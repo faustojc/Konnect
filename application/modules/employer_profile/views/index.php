b8ed5d9be4f0d4f99df877cb15681a67e7ed8e4a
kaypajanconi <kpajanconi7@gmail.com>
2023-07-17 10:22:47 +0800

    Update index.php

--- application/modules/employer_profile/views/index.php
+++ application/modules/employer_profile/views/index.php
@@ -179,6 +179,28 @@ main_header(['Employer_profile']);
                                     </div>
                                 </div>
                             </div>
+
+                            <!-- Navbar -->
+                            <div class="col-12 mt-4">
+                                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                                    <div class="container">
+                                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                            <span class="navbar-toggler-icon"></span>
+                                        </button>
+                                        <div class="collapse navbar-collapse" id="navbarNav">
+                                            <ul class="navbar-nav">
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="#">Overview</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="#">Jobs</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </nav>
+                            </div>
+
                         </div>
                     </div>
                 </div>

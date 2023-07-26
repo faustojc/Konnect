3d8ae1c5da87718d72639f99a349ee1ff7a9ce04
faustojc <faustojcboko@gmail.com>
2023-07-26 22:49:42 +0800

    Update beu_database.sql

--- db/beu_database.sql
+++ db/beu_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 25, 2023 at 07:18 PM
+-- Generation Time: Jul 26, 2023 at 04:49 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 

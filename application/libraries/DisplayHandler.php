4ad32a83851b669e70de3b2e4904b602dc12f57d
faustojc <faustojcboko@gmail.com>
2023-07-28 23:58:28 +0800

    Update DisplayHandler.php

--- application/libraries/DisplayHandler.php
+++ application/libraries/DisplayHandler.php
@@ -1,20 +1,19 @@
 <?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
 class DisplayHandler
 {
     private CI_Controller $CI;
     private $userdata;
-    private $auth;
 
     public function __construct()
     {
         $this->CI = &get_instance();
 
         $this->userdata = get_userdata(USER);
-        $this->auth = get_userdata(AUTH);
     }
 
-    public function updateNotification()
+    public function updateNotification(): void
     {
         $notifications = $this->CI->Notification_model->getNotifications($this->userdata->user_id);
 

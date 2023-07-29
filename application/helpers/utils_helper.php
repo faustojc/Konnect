510ba35c526147224bab7c9625072c745d1ecda2
faustojc <faustojcboko@gmail.com>
2023-07-29 22:16:45 +0800

    Added formatTime func and remove unused func

--- application/helpers/utils_helper.php
+++ application/helpers/utils_helper.php
@@ -12,93 +12,40 @@ if (!function_exists('image_exist')) {
     }
 }
 
-function select_active($value1, $value2)
-{
-    return ($value1 == $value2) ? "selected='selected'" : "";
-}
+if (!function_exists('formatTime')) {
+    function formatTime(string $time): string
+    {
+        $timeDiff = time() - strtotime($time);
 
-function disable_field($id)
-{
-    if ($id > 0) {
-        return "readonly='readonly'";
-    }
-}
-
-function user_module($user_modules, $module_ID)
-{
-    foreach ($user_modules as $item) {
-        if ($item->PersonnelType_ID == $module_ID) {
-            // if ($item->$permission == 1)
-            // {
-            return "hidden";
-            // }
+        if ($timeDiff < 60) {
+            $formattedTimeDiff = 'less than a minute ago';
+        } else if ($timeDiff < 3600) {
+            $minutes = floor($timeDiff / 60);
+            $formattedTimeDiff = $minutes . ' minute' . ($minutes > 1 ? 's' : '') . ' ago';
+        } else if ($timeDiff < 86400) {
+            $hours = floor($timeDiff / 3600);
+            $formattedTimeDiff = $hours . ' hour' . ($hours > 1 ? 's' : '') . ' ago';
+        } else if ($timeDiff < 604800) {
+            $days = floor($timeDiff / 86400);
+            $formattedTimeDiff = $days . ' day' . ($days > 1 ? 's' : '') . ' ago';
+        } else if ($timeDiff < 2592000) {
+            $weeks = floor($timeDiff / 604800);
+            $formattedTimeDiff = $weeks . ' week' . ($weeks > 1 ? 's' : '') . ' ago';
+        } else {
+            $months = floor($timeDiff / 2592000);
+            $formattedTimeDiff = $months . ' month' . ($months > 1 ? 's' : '') . ' ago';
         }
+
+        return $formattedTimeDiff;
     }
 }
 
-function format_date($date)
-{
-    $date = explode("-", $date);
-    $month = $date[1];
-    switch ($date[1]) {
-        case "01":
-            $month = "January";
-            break;
-        case "02":
-            $month = "February";
-            break;
-        case "03":
-            $month = "March";
-            break;
-        case "04":
-            $month = "April";
-            break;
-        case "05":
-            $month = "May";
-            break;
-        case "06":
-            $month = "June";
-            break;
-        case "07":
-            $month = "July";
-            break;
-        case "08":
-            $month = "August";
-            break;
-        case "09":
-            $month = "September";
-            break;
-        case "10":
-            $month = "October";
-            break;
-        case "11":
-            $month = "November";
-            break;
-        case "12":
-            $month = "December";
-            break;
-    }
-
-    return $month . " " . $date[2] . ", " . $date[0];
-}
-
-function is_empty_object($obj)
+function is_empty_object($obj): bool
 {
     return $obj == new stdClass();
 }
 
-function socket_url()
-{
-    $ci =& get_instance();
-    return $ci->config->item('socket_url');
-}
-
-function generate_random_integer()
-{
-    return bin2hex(random_bytes(4));
-}
-
-function sidebar($sidebar = NULL, $realSidebar = NULL)
+function sidebar($sidebar = NULL, $realSidebar = NULL): bool
 {
     //--NOTE--
     // Array values are key sensitive.
@@ -119,14 +66,14 @@ function sidebar($sidebar = NULL, $realSidebar = NULL)
     }
 
     do {
-        if (!isset($sidebar[$counter]) || !isset($realSidebar[$counter])) {
+        if (!isset($sidebar[$counter], $realSidebar[$counter])) {
             $sidebar[$counter] = NULL;
             $realSidebar[$counter] = NULL;
         }
         if ($sidebar[$counter] == $realSidebar[$counter]) {
-            array_push($result, TRUE);
+            $result[] = TRUE;
         } else {
-            array_push($result, FALSE);
+            $result[] = FALSE;
         }
         $counter++;
         if ($counter == $countSidebar) {
@@ -135,30 +82,4 @@ function sidebar($sidebar = NULL, $realSidebar = NULL)
     } while ($flag);
 
     return count(array_keys($result, TRUE)) == count($result);
-}
-
-/** user logs */
-function user_logs($dataValue)
-{
-
-    $CI =& get_instance();
-    $secret_key = $CI->config->item('encryption_key');
-    $table = 'ctc_user_logs';
-
-    $non_encrypt_data = [
-        'origin_id' => $dataValue['originID'],
-        'receiver_id' => $dataValue['receiverID'],
-        'field_value' => str_replace('"', "'", serialize($dataValue['field_value'])),
-        'date_created' => date('Y-m-d H:i:s'),
-    ];
-
-    $encrypt_data = [
-        'table_origin' => $dataValue['tableOrigin'],
-        'origin_name' => $dataValue['originName'],
-        'table_receiver' => $dataValue['tableReceiver'],
-        'receiver_name' => $dataValue['receiverName'],
-        'description' => $dataValue['description'],
-    ];
-
-    insert_encryted_data($table, $encrypt_data, $non_encrypt_data);
 }

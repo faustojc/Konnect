06fe537b9f68065b0e791a1e659ad264ad2a066d
faustojc <faustojcboko@gmail.com>
2023-07-31 21:59:23 +0800

    Load only the search content

--- assets/js/search/index.js
+++ assets/js/search/index.js
@@ -28,7 +28,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-
 // Function to handle the search on Enter key press
 function handleSearch() {
     const searchInput = document.getElementById('searchInput');
@@ -37,28 +36,20 @@ function handleSearch() {
 
     localStorage.setItem('searchTerm', searchTerm);
 
-    window.location.href = baseUrl + 'search?query=' + encodeURIComponent(searchTerm);
+    //window.location.href = baseUrl + 'search?query=' + encodeURIComponent(searchTerm);
 
-    // const url = window.location.href;
-    // if (url.includes('search')) {
-    //     const query = searchInput.value;
-    //
-    //     // check if the query is empty
-    //     if (query !== '') {
-    //         fetch(baseUrl + 'search?query=' + query, {
-    //             headers: {
-    //                 'X-Requested-With': 'XMLHttpRequest'
-    //             }
-    //         })
-    //             .then(response => response.text())
-    //             .then(data => {
-    //                 const search_result_content = document.querySelector('#search_result_content');
-    //                 // Display the search result and search time in the search page
-    //                 search_result_content.innerHTML = data;
-    //             });
-    //
-    //     }
-    // } else if (searchTerm.trim() !== '') {
-    //     window.location.href = baseUrl + 'search?query=' + encodeURIComponent(searchTerm);
-    // }
+    const url = window.location.href;
+    if (url.includes('search')) {
+        if (searchTerm !== '') {
+            fetch(baseUrl + 'search?query=' + searchTerm, {headers: {'X-Requested-With': 'XMLHttpRequest'}})
+                .then(response => response.text())
+                .then(data => {
+                    const search_result_content = document.querySelector('#search_result_content');
+                    // Display the search result and search time in the search page
+                    search_result_content.innerHTML = data;
+                });
+        }
+    } else if (searchTerm.trim() !== '') {
+        window.location.href = baseUrl + 'search?query=' + encodeURIComponent(searchTerm);
+    }
 }

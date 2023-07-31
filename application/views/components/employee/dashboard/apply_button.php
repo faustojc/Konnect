3df1f1b6c59cb61e5c3ebd41c130aca91089f014
faustojc <faustojcboko@gmail.com>
2023-07-31 21:57:51 +0800

    Added <hr>

--- application/views/components/employee/dashboard/apply_button.php
+++ application/views/components/employee/dashboard/apply_button.php
@@ -1,5 +1,6 @@
 <div class="row">
     <div class="col-lg-12 col-md-4 col-sm-12">
+        <hr>
         <button type="button" class="btn btn-light btn-block apply-button" data-id="<?= $job_id ?>"><?= $status ?></button>
     </div>
 </div>

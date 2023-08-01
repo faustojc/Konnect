c4bb5bfb78b409037338cf7a330fd8fe6569523c
faustojc <faustojcboko@gmail.com>
2023-08-01 12:09:21 +0800

    Update Employee_services_model.php

--- application/modules/employee/models/service/Employee_services_model.php
+++ application/modules/employee/models/service/Employee_services_model.php
@@ -1,5 +1,6 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
+
 class Employee_services_model extends CI_Model
 {
     public $ID;
@@ -8,7 +9,7 @@ class Employee_services_model extends CI_Model
     public function __construct()
     {
         parent::__construct();
-        $this->session = (object) get_userdata(USER);
+        $this->session = (object)get_userdata(USER);
 
         // if(is_empty_object($this->session)){
         // 	redirect(base_url().'login/authentication', 'refresh');
@@ -22,7 +23,7 @@ class Employee_services_model extends CI_Model
     public function register()
     {
         try {
-            $data = array(
+            $data = [
 
                 'Fname' => $this->Fname,
                 'Mname' => $this->Mname,
@@ -41,9 +42,9 @@ class Employee_services_model extends CI_Model
                 'Tin' => $this->Tin,
                 'Phil_health' => $this->Phil_health,
                 'Pag_ibig' => $this->Pag_ibig,
-                'Date_created' => date('Y-m-d H:i:s')
+                'Date_created' => date('Y-m-d H:i:s'),
 
-            );
+            ];
             echo json_encode($data);
             $this->db->trans_start();
 
@@ -52,20 +53,20 @@ class Employee_services_model extends CI_Model
             $this->db->trans_complete();
             if ($this->db->trans_status() === FALSE) {
                 $this->db->trans_rollback();
-                throw new Exception(ERROR_PROCESSING, true);
+                throw new Exception(ERROR_PROCESSING, TRUE);
             } else {
                 $this->db->trans_commit();
-                return array('message' => SAVED_SUCCESSFUL, 'has_error' => false);
+                return ['message' => SAVED_SUCCESSFUL, 'has_error' => FALSE];
             }
         } catch (Exception $msg) {
-            return (array('message' => $msg->getMessage(), 'has_error' => true));
+            return (['message' => $msg->getMessage(), 'has_error' => TRUE]);
         }
     }
 
     public function update()
     {
         try {
-            $data = array(
+            $data = [
                 'ID' => $this->employee_ID,
                 'Fname' => $this->Fname,
                 'Mname' => $this->Mname,
@@ -83,8 +84,8 @@ class Employee_services_model extends CI_Model
                 'SSS' => $this->SSS,
                 'Tin' => $this->Tin,
                 'Phil_health' => $this->Phil_health,
-                'Pag_ibig' => $this->Pag_ibig
-            );
+                'Pag_ibig' => $this->Pag_ibig,
+            ];
 
             if (!empty($this->Employee_image)) {
                 $data['Employee_image'] = $this->Employee_image;
@@ -98,29 +99,13 @@ class Employee_services_model extends CI_Model
 
             if ($this->db->trans_status()) {
                 $this->db->trans_commit();
-                return array('message' => SAVED_SUCCESSFUL, 'has_error' => false);
+                return ['message' => SAVED_SUCCESSFUL, 'has_error' => FALSE];
             } else {
                 $this->db->trans_rollback();
-                throw new Exception(ERROR_PROCESSING, true);
+                throw new Exception(ERROR_PROCESSING, TRUE);
             }
         } catch (Exception $e) {
-            return array('message' => $e->getMessage(), 'has_error' => true);
+            return ['message' => $e->getMessage(), 'has_error' => TRUE];
         }
     }
-
-
-    public function search_employee()
-    {
-        try {
-            $result = $this->db->select()->from($this->Table->employee)
-                ->like('Fname', $this->search_text)
-                ->or_like('Lname', $this->search_text)
-                ->get()->result();
-
-            return $result;
-        } catch (Exception $e) {
-            return array('message' => $e->getMessage(), 'has_error' => true);
-        }
-    }
-
 }

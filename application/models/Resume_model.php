206e216a2d4c7b33020e9a9ee202204c263704a4
faustojc <faustojcboko@gmail.com>
2023-08-03 16:26:44 +0800

    Create Resume_model.php

---
+++ application/models/Resume_model.php
@@ -0,0 +1,91 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ * Relationship with employee: ONE TO ONE
+ */
+class Resume_model extends CI_Model
+{
+    /**
+     * @var mixed
+     */
+    private $Table;
+
+    /**
+     * @throws JsonException
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->Table = json_decode(TABLE, FALSE, 512, JSON_THROW_ON_ERROR);
+    }
+
+    /**
+     * @param int $employee_id
+     *
+     * @return array|mixed|object|null
+     */
+    public function getEmployeesResume(int $employee_id)
+    {
+        return $this->db->get_where($this->Table->resume, ['employee_id' => $employee_id])->row();
+    }
+
+    public function save($info): array
+    {
+        try {
+            $this->db->trans_start();
+            $this->db->insert($this->Table->employee_skill, $info);
+            $this->db->trans_complete();
+
+            if ($this->db->trans_status() === FALSE) {
+                $this->db->trans_rollback();
+                return ['message' => ERROR_PROCESSING, 'has_error' => TRUE];
+            }
+
+            $this->db->trans_commit();
+            return ['message' => SAVED_SUCCESSFUL, 'has_error' => FALSE];
+        } catch (Exception $msg) {
+            return ['message' => $msg->getMessage(), 'has_error' => TRUE];
+        }
+    }
+
+    public function update($id, $data): array
+    {
+        try {
+            $this->db->trans_start();
+            $this->db->where('id', $id)->or_where('employee_id', $id)->update($this->Table->employee_skill, $data);
+            $this->db->trans_complete();
+
+            if ($this->db->trans_status() === FALSE) {
+                $this->db->trans_rollback();
+                return ['message' => ERROR_PROCESSING, 'has_error' => TRUE];
+            }
+
+            $this->db->trans_commit();
+            return ['message' => UPDATE_SUCCESSFUL, 'has_error' => FALSE];
+
+        } catch (Exception $msg) {
+            return (['message' => $msg->getMessage(), 'has_error' => TRUE]);
+        }
+    }
+
+    public function delete($id): array
+    {
+        try {
+            $this->db->trans_start();
+            $this->db->where('id', $id)->or_where('employee_id')->delete($this->Table->resume);
+            $this->db->trans_complete();
+
+            if ($this->db->trans_status() === FALSE) {
+                $this->db->trans_rollback();
+                throw new RuntimeException(ERROR_PROCESSING, TRUE);
+            }
+
+            $this->db->trans_commit();
+            return ['message' => DELETED_SUCCESSFUL, 'has_error' => FALSE];
+        } catch (Exception $msg) {
+            return (['message' => $msg->getMessage(), 'has_error' => TRUE]);
+        }
+    }
+}

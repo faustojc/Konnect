dcf32631895ef72f9ecef1c882502193afa4cac7
faustojc <faustojcboko@gmail.com>
2023-08-07 21:06:50 +0800

    Update tbl_education.sql

--- db/ddl/beu_database/tbl_education.sql
+++ db/ddl/beu_database/tbl_education.sql
@@ -10,7 +10,7 @@ CREATE TABLE beu_database.tbl_education
     start_date  DATE          NOT NULL,
     end_date    DATE          NULL,
     CONSTRAINT fk_Employee_ID
-        FOREIGN KEY (Employee_id) REFERENCES beu_database.tbl_employee (ID)
+        FOREIGN KEY (employee_id) REFERENCES beu_database.tbl_employee (ID)
             ON UPDATE CASCADE ON DELETE CASCADE
 )
     CHARSET = latin1;

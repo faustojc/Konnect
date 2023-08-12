64ec0c32c33d0e22531afec405cd8f8f31bf3f77
faustojc <faustojcboko@gmail.com>
2023-08-12 21:37:07 +0800

    Added ssr entry file in vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import {defineConfig} from 'vite';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
+            ssr: 'resources/js/ssr.js',
             refresh: true,
         }),
     ],

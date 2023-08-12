09cf940f6d31f2c0594ee20435c9255e57695717
faustojc <faustojcboko@gmail.com>
2023-08-12 21:34:12 +0800

    Added Inertia with SSR

---
+++ resources/js/ssr.js
@@ -0,0 +1,15 @@
+import {createInertiaApp} from '@inertiajs/react'
+import createServer from '@inertiajs/react/server'
+import {renderToString} from 'react-dom/server'
+
+createServer(page =>
+    createInertiaApp({
+        page,
+        render: renderToString,
+        resolve: name => {
+            const pages = import.meta.glob('./Pages/**/*.jsx');
+            return pages[`./Pages/${name}.jsx`]();
+        },
+        setup: ({App, props}) => <App {...props} />,
+    }),
+)

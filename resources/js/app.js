a20c7aeacf956a94ffc87df0c04bb86d1cb9e634
faustojc <faustojcboko@gmail.com>
2023-08-12 21:36:55 +0800

    Inertia hydration root for SSR

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-require('./bootstrap');
+import {createInertiaApp} from '@inertiajs/react';
+import {hydrateRoot} from 'react-dom/client';
+
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.jsx');
+        return pages[`./Pages/${name}.jsx`]();
+    },
+    setup({el, App, props}) {
+        hydrateRoot(el, <App {...props} />);
+    },
+}).then(r => console.log(r));

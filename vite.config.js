c1a5bb71c71142b06f6384ea2b6c6c4237bc37bf
faustojc <faustojcboko@gmail.com>
2023-08-13 09:35:14 +0800

    Renamed file type to jsx

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import {defineConfig} from 'vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            ssr: 'resources/js/ssr.js',
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            ssr: 'resources/js/ssr.jsx',
             refresh: true,
         }),
     ],
